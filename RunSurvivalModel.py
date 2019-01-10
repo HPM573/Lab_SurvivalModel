886c8607ce92912be4dd77a87b91a93dce33362f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-01-10 14:59:57 -0500

    patient

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,31 +1,13 @@
 import SurvivalModelClasses as Cls
-import SimPy.SamplePathClasses as SamplePathSupport
-import SimPy.FigureSupport as Fig
 
-MORTALITY_PROB = 0.1    # annual probability of mortality
-TIME_STEPS = 100        # simulation length
-SIM_POP_SIZE = 1000     # population size of the simulated cohort
+MORTALITY_PROB = 0.1
+TIME_STEPS = 40
 
-# create a cohort of patients
-myCohort = Cls.Cohort(id=1, pop_size=SIM_POP_SIZE, mortality_prob=MORTALITY_PROB)
+# create a patient
+myPatient = Cls.Patient(id=1, mortality_prob=MORTALITY_PROB)
 
-# simulate the cohort
-myCohort.simulate(n_time_steps=TIME_STEPS)
-
-# plot the sample path
-SamplePathSupport.graph_sample_path(
-    sample_path=myCohort.cohortOutcomes.nLivingPatients,
-    title='Survival Curve',
-    x_label='Time-Step (Year)',
-    y_label='Number Survived')
-
-# plot the histogram
-Fig.graph_histogram(
-    data=myCohort.cohortOutcomes.survivalTimes,
-    title='Histogram of Patient Survival Time',
-    x_label='Survival Time (Year)',
-    y_label='Count')
+# simulate the patient over the specified time steps
+myPatient.simulate(TIME_STEPS)
 
 # print the patient survival time
-print('Mean survival time (years):',
-      myCohort.cohortOutcomes.meanSurvivalTime)
+print(myPatient.survivalTime)
