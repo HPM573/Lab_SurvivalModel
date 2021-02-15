4f1c8111d7d95ea76bc2d89148780f53674d5d6a
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-02-15 10:43:27 -0500

    P1

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,13 +1,13 @@
-import SurvivalModelClasses as Cls
+from SurvivalModelClasses import Patient
 
 MORTALITY_PROB = 0.1    # annual probability of death
 TIME_STEPS = 40         # years
 
 # create a patient
-myPatient = Cls.Patient(id=1, mortality_prob=MORTALITY_PROB)
+myPatient = Patient(id=1, mortality_prob=MORTALITY_PROB)
 
 # simulate the patient over the specified time steps
-myPatient.simulate(TIME_STEPS)
+myPatient.simulate(n_time_steps=TIME_STEPS)
 
 # print the patient survival time
 print(myPatient.survivalTime)
