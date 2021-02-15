8bbc72579c9706e04b43b44f185354f3a5b0fa02
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-02-15 10:48:35 -0500

    p2

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,10 +1,10 @@
-import SurvivalModelClasses as Cls
+from SurvivalModelClasses import Patient
 
 MORTALITY_PROB = 0.1    # annual probability of death
 TIME_STEPS = 40         # years
 
 # create a patient
-myPatient = Cls.Patient(id=1, mortality_prob=MORTALITY_PROB)
+myPatient = Patient(id=1, mortality_prob=MORTALITY_PROB)
 
 # simulate the patient over the specified time steps
 myPatient.simulate(TIME_STEPS)
--- SurvivalModelClasses.py
+++ SurvivalModelClasses.py
@@ -15,7 +15,6 @@ class Patient:
         :param mortality_prob: probability of death during a time-step (must be in [0,1])
         """
         self.id = id
-        self.rng = None  # random number generator for this patient
         self.mortalityProb = mortality_prob
         self.healthState = HealthStat.ALIVE  # assuming all patients are alive at the beginning
         self.survivalTime = None   # won't be observed unless the patient dies
@@ -23,15 +22,15 @@ class Patient:
     def simulate(self, n_time_steps):
         """ simulate the patient over the specified simulation length """
 
-        # assign the patient's random number generator
-        self.rng = np.random.RandomState(seed=self.id)
+        # random number generator
+        rng = np.random.RandomState(seed=self.id)
 
         t = 0  # simulation current time
 
         # while the patient is alive and simulation length is not yet reached
         while self.healthState == HealthStat.ALIVE and t < n_time_steps:
             # determine if the patient will die during this time-step
-            if self.rng.random_sample() < self.mortalityProb:
+            if rng.random_sample() < self.mortalityProb:
                 # update the health state to death
                 self.healthState = HealthStat.DEAD
                 # record the survival time (assuming deaths occurs at the end of this period)
