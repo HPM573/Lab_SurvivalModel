70a35b7e7cb16e814ce19384a4e6e0074fae7a84
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-12-19 22:52:57 -0500

    2023

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,5 +1,5 @@
-import deampy.plots.histogram as Hist
-import deampy.plots.sample_paths as Path
+import deampy.plots.histogram as hist
+import deampy.plots.sample_paths as path
 
 from SurvivalModelClasses import Cohort
 
@@ -14,20 +14,21 @@ myCohort = Cohort(id=1, pop_size=POP_SIZE, mortality_prob=MORTALITY_PROB)
 myCohort.simulate(n_time_steps=TIME_STEPS)
 
 # plot the sample path
-Path.plot_sample_path(
+path.plot_sample_path(
     sample_path=myCohort.cohortOutcomes.nLivingPatients,
     title='Survival Curve',
     x_label='Simulation Year',
     y_label='Number Alive',
     figure_size=(4, 3.6),
+    color='blue', transparency=0.5,
     file_name='figs/survival_curve.png')
 
 # plot the histogram
-Hist.plot_histogram(
+hist.plot_histogram(
     data=myCohort.cohortOutcomes.survivalTimes,
     title='Histogram of Patient Survival Time',
     x_label='Survival Time (Year)',
-    y_label='Count',
+    color='blue', transparency=0.5,
     bin_width=2,
     figure_size=(4, 3.6),
     file_name='figs/histogram.png')
