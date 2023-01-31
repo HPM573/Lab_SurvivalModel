da5d87221c28de08a0fd08c9be6cecf53aa5e93a
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-01-31 10:41:09 -0500

    adding cohort branch

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,6 +1,3 @@
-import deampy.plots.histogram as hist
-import deampy.plots.sample_paths as path
-
 from SurvivalModelClasses import Cohort
 
 MORTALITY_PROB = 0.1    # annual probability of death
@@ -13,26 +10,6 @@ myCohort = Cohort(id=1, pop_size=POP_SIZE, mortality_prob=MORTALITY_PROB)
 # simulate the cohort over the specified time steps
 myCohort.simulate(n_time_steps=TIME_STEPS)
 
-# plot the sample path
-path.plot_sample_path(
-    sample_path=myCohort.cohortOutcomes.nLivingPatients,
-    title='Survival Curve',
-    x_label='Simulation Year',
-    y_label='Number Alive',
-    figure_size=(4, 3.6),
-    color='blue', transparency=0.5,
-    file_name='figs/survival_curve.png')
+# print mean survival time
+print('Mean survival time:', myCohort.meanSurvivalTime)
 
-# plot the histogram
-hist.plot_histogram(
-    data=myCohort.cohortOutcomes.survivalTimes,
-    title='Histogram of Patient Survival Time',
-    x_label='Survival Time (Year)',
-    color='blue', transparency=0.5,
-    bin_width=2,
-    figure_size=(4, 3.6),
-    file_name='figs/histogram.png')
-
-# print the patient survival time
-print('Mean survival time (years):',
-      myCohort.cohortOutcomes.meanSurvivalTime)
--- SurvivalModelClasses.py
+++ SurvivalModelClasses.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 import numpy as np
-from deampy.sample_path import PrevalencePathBatchUpdate
 
 
 class HealthStat(Enum):
@@ -52,12 +51,14 @@ class Cohort:
         self.id = id
         self.popSize = pop_size  # initial population size
         self.mortalityProb = mortality_prob
-        self.cohortOutcomes = CohortOutcomes()  # outcomes of the simulated cohort
+        self.meanSurvivalTime = None    # mean survival times
 
     def simulate(self, n_time_steps):
         """ simulate the cohort of patients over the specified number of time-steps
         :param n_time_steps: number of time steps to simulate the cohort
         """
+
+        survival_times = [] # patient survival times
 
         # populate and simulate the cohort
         for i in range(self.popSize):
@@ -67,40 +68,10 @@ class Cohort:
             # simulate
             patient.simulate(n_time_steps)
 
-            # store outputs of this simulation
-            self.cohortOutcomes.extract_outcome(patient)
-
-        # calculate cohort outcomes
-        self.cohortOutcomes.calculate_cohort_outcomes(initial_pop_size=self.popSize)
-
-
-class CohortOutcomes:
-    def __init__(self):
-
-        self.survivalTimes = []    # survival times
-        self.meanSurvivalTime = None   # mean survival time
-        self.nLivingPatients = None   # survival curve (sample path of number of alive patients over time)
-
-    def extract_outcome(self, simulated_patient):
-        """ extracts outcomes of a simulated patient
-        :param simulated_patient: a patient after being simulated """
-
-        # record survival times
-        if simulated_patient.survivalTime is not None:
-            self.survivalTimes.append(simulated_patient.survivalTime)
-
-    def calculate_cohort_outcomes(self, initial_pop_size):
-        """ calculates the cohort outcomes
-        :param initial_pop_size: initial population size
-        """
+            # record survival times
+            if patient.survivalTime is not None:
+                survival_times.append(patient.survivalTime)
 
         # calculate mean survival time
-        self.meanSurvivalTime = sum(self.survivalTimes)/len(self.survivalTimes)
+        self.meanSurvivalTime = sum(survival_times)/len(survival_times)
 
-        # survival curve
-        self.nLivingPatients = PrevalencePathBatchUpdate(
-            name='# of living patients',
-            initial_size=initial_pop_size,
-            times_of_changes=self.survivalTimes,
-            increments=[-1]*len(self.survivalTimes)
-        )
